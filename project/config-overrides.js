04cbfc0675f52deedf2c52317ba4117bdac698e4
李昊谦 <18060784@suning.com>
2021-03-05 22:35:12 +0800

    初次提交

---
+++ project/config-overrides.js
@@ -0,0 +1,19 @@
+/* config-overrides.js */
+const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true,
+  }),
+
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: {
+        '@primary-color': '#2daebf',
+      },
+    },
+  })
+);
