9906f0ffe6bd56b833fcdc701574d1a8469320ec
Hoian <lhq_wz@veazer.com>
2021-08-26 00:15:24 +0800

    完善 tool bar

--- project-vue/src/components/toolbar/Toolbar.vue
+++ project-vue/src/components/toolbar/Toolbar.vue
@@ -1,20 +1,64 @@
 <template>
 	<div class="toolbar">
-		<div @click="mg">123</div>
+		<div class="toolbar-item" v-for="(item, index) in menuList" :key="index">
+			<div class="toolbar-item-icon">
+				<img v-show="item.type == curMenuMain" :src="item.icon" />
+				<img v-show="item.type != curMenuMain" :src="item.actIcon" />
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	created() {},
-	methods: {
-		mg() {
-			this.$notify({
-				title: "标题名称",
-				message: "这是一条不会自动关闭的消息",
-			});
-		},
+	data() {
+		return {
+			curMenu: "",
+			curMenuMain: "pencil",
+			menuList: [
+				{
+					name: "笔",
+					type: "pencil",
+					icon: "./img/pencil.png",
+					actIcon: "./img/pencil-act.png",
+				},
+				{
+					name: "线",
+					type: "line",
+					icon: "./img/line.png",
+					actIcon: "./img/line-act.png",
+					children: [
+						{
+							name: "直线",
+							type: "straight-line",
+							icon: "",
+							actIcon: "",
+						},
+					],
+				},
+				{
+					name: "文字",
+					type: "text",
+					icon: "./img/text.png",
+					actIcon: "./img/text-act.png",
+				},
+				{
+					name: "图片",
+					type: "img",
+					icon: "./img/img.png",
+					actIcon: "./img/img-act.png",
+				},
+				{
+					name: "文件",
+					type: "file",
+					icon: "./img/file.png",
+					actIcon: "./img/file-act.png",
+				},
+			],
+		};
 	},
+	created() {},
+	methods: {},
 };
 </script>
 
@@ -26,5 +70,21 @@ export default {
 	width: 50px;
 	background-color: #fff;
 	border-radius: 6px;
+	cursor: pointer;
+
+	.toolbar-item {
+		width: 40px;
+		height: 40px;
+
+		.toolbar-item-icon {
+			width: 40px;
+			height: 40px;
+
+			& > img {
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
 }
 </style>
